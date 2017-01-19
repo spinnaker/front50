ddc03ba474181fbf1f2c2aa6d279e30cc16df3d3
Greg Turnquist <gturnquist@pivotal.io>
2017-01-19 16:21:01 -0600

    Add Spring Cloud Config Server as optional source of properties
    * By default, config server is disabled. Enable with spring.cloud.config.server.bootstrap=true
    * It defaults to being served underneath /config, but that can be optionally overridden.

---
+++ front50-web/src/main/groovy/com/netflix/spinnaker/front50/config/SpringCloudConfigServerConfig.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 Pivotal, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.front50.config
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Create an embedded config server.
+ *
+ * NOTE: It's opt-in via spring.cloud.config.server.bootstrap=true. Also requires
+ *       spring.cloud.config.server.git.uri or spring.cloud.config.server.svn.uri
+ */
+@Configuration
+@ConditionalOnProperty('spring.cloud.config.server.bootstrap')
+class SpringCloudConfigServerConfig {
+
+  @EnableConfigServer
+  static class SpringCloudEmbeddedConfigServer {}
+
+}
