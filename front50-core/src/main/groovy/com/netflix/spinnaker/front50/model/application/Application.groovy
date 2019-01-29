spinnaker/spinnaker

commit 23090d5a32a7e254f65279d1f1555a958fc0f138

refactor(application): ensure cloudProviders is a comma-separated string (#427)

front50-core/src/main/groovy/com/netflix/spinnaker/front50/model/application/Application.groovy
@@ -53,9 +53,15 @@ class Application implements Timestamped {
   String updateTs
   String createTs
   String lastModifiedBy
+  Object cloudProviders // might be persisted as a List or a String
   public List<TrafficGuard> trafficGuards = []
 
   private Map<String, Object> details = new HashMap<String, Object>()
+
+  String getCloudProviders() {
+    // Orca expects a String
+    return cloudProviders instanceof List ? cloudProviders.join(',') : cloudProviders
+  }
 
   String getName() {
     // there is an expectation that application names are uppercased (historical)

front50-core/src/test/groovy/com/netflix/spinnaker/front50/model/application/ApplicationModelSpec.groovy
@@ -228,6 +228,15 @@ class ApplicationModelSpec extends Specification {
     apps.size() == 2
   }
 
+  void 'should convert cloudProviders to a String if it is a list'() {
+    def listApp = new Application(cloudProviders: ['a','b'])
+    def normalApp = new Application(cloudProviders: 'a,b,c')
+
+    expect:
+    listApp.cloudProviders == 'a,b'
+    normalApp.cloudProviders == 'a,b,c'
+  }
+
   void 'should return empty collection if no apps exist'() {
     def dao = Mock(ApplicationDAO) {
       1 * all() >> { throw new NotFoundException("no apps found") }

spinnaker/front50

commit 12915579222f42bc192f157201931a9fabb6258f

fix(application): Persist cloud providers.
Broke in commit 23090d5a32a7e254f65279d1f1555a958fc0f138

front50-core/src/main/groovy/com/netflix/spinnaker/front50/model/application/Application.groovy
@@ -84,6 +84,7 @@ class Application implements Timestamped {
         name: this.name,
         description: this.description,
         email: this.email,
+        cloudProviders: this.cloudProviders,
         updateTs: this.updateTs,
         createTs: this.createTs,
         details: this.details