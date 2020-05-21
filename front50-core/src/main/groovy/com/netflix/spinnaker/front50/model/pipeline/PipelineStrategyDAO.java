c24558a153e32de66deb00e699be1e087e6204ec
Rob Zienert <rzienert@netflix.com>
2020-05-21 12:49:04 -0700

    refactor(core): PipelineStrategyDAO to Java

--- front50-core/src/main/groovy/com/netflix/spinnaker/front50/model/pipeline/PipelineStrategyDAO.java
+++ front50-core/src/main/groovy/com/netflix/spinnaker/front50/model/pipeline/PipelineStrategyDAO.java
@@ -3,10 +3,8 @@ package com.netflix.spinnaker.front50.model.pipeline;
 import com.netflix.spinnaker.front50.model.ItemDAO;
 import java.util.Collection;
 
-public interface PipelineDAO extends ItemDAO<Pipeline> {
+public interface PipelineStrategyDAO extends ItemDAO<Pipeline> {
   String getPipelineId(String application, String pipelineName);
 
   Collection<Pipeline> getPipelinesByApplication(String application);
-
-  Collection<Pipeline> getPipelinesByApplication(String application, boolean refresh);
 }
