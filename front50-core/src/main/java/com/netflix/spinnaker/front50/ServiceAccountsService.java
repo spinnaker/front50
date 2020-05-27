b45c72754cbb3f33aef87b5e11b0ce3de23dbb25
Jørgen Jervidalo <jorgen.jervidalo@schibsted.com>
2020-05-27 16:17:16 +0200

    This was a bit of a weird error. After we turned off the S3 provider in Front50, the `/serviceAccounts` endpoint stopped working, failing with the following error message: "Configured storage service does not support service account permissions". This happens when there's no `ServiceAccountsService` bean (which it really wasn't). This bean is autoconfigured using `@ConditionalOnBean(ServiceAccountDAO.class)`, and according to Spring there's no ServiceAccountDAO in the context. But when inspecting the context, it actually _is_ a `ServiceAccountDAO` present. I think that for some reason the `ServiceAccountDAO` bean is created _after_ Spring tries to wire up `ServiceAccountsService`, but this doesn't happen with the other providers. I have no idea why. The only thing I can think of is that `SqlConfiguration.kt` is the first provider config written in Kotlin, but I don't really understand why that should matter. I'd appreciate any insight into the matter.
    I have fixed the issue by creating a new `@ConditionalOnAnyProviderExceptRedisIsEnabled` annotation that returns true if any of the providers except Redis are enabled. I've tested running the app with only SQL enabled, only Redis enabled*, and both SQL and Redis enabled. All works now.
    
    \* This didn't work before, as @robzienert correctly theorised.

--- front50-core/src/main/java/com/netflix/spinnaker/front50/ServiceAccountsService.java
+++ front50-core/src/main/java/com/netflix/spinnaker/front50/ServiceAccountsService.java
@@ -33,8 +33,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import retrofit.RetrofitError;
 
+@Service
 public class ServiceAccountsService {
   private static final Logger log = LoggerFactory.getLogger(ServiceAccountsService.class);
   private static final String MANAGED_SERVICE_ACCOUNT_SUFFIX = "@managed-service-account";
