074b7bec6e26d30aa3b3576f73bf159cf254267b
Rob Zienert <rzienert@netflix.com>
2020-05-27 00:08:22 -0700

    refactor(core): Pipeline to Java

--- front50-core/src/main/groovy/com/netflix/spinnaker/front50/model/pipeline/Pipeline.java
+++ front50-core/src/main/groovy/com/netflix/spinnaker/front50/model/pipeline/Pipeline.java
@@ -1,124 +1,118 @@
-/*
- * Copyright 2016 Netflix, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.netflix.spinnaker.front50.model.pipeline;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import com.netflix.spinnaker.front50.model.Timestamped;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 
-package com.netflix.spinnaker.front50.model.pipeline
+public class Pipeline extends HashMap<String, Object> implements Timestamped {
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.netflix.spinnaker.front50.model.Timestamped
-
-class Pipeline extends HashMap<String, Object> implements Timestamped {
-
-  private static ObjectMapper mapper = new ObjectMapper()
-
-  public static final String TYPE_TEMPLATED = "templatedPipeline"
+  private static ObjectMapper MAPPER = new ObjectMapper();
+  public static final String TYPE_TEMPLATED = "templatedPipeline";
 
   @JsonIgnore
-  String getApplication() {
-    return super.get("application")
+  public String getApplication() {
+    return (String) super.get("application");
   }
 
   @JsonIgnore
-  String getName() {
-    return super.get("name")
+  public String getName() {
+    return (String) super.get("name");
   }
 
-  void setName(String name) {
-    super.put("name", name)
+  public void setName(String name) {
+    super.put("name", name);
   }
 
   @Override
   @JsonIgnore
-  String getId() {
-    return super.get("id")
+  public String getId() {
+    return (String) super.get("id");
   }
 
-  void setId(String id) {
-    super.put("id", id)
-  }
-
-  @Override
-  @JsonIgnore
-  Long getLastModified() {
-    def updateTs = super.get("updateTs") as String
-    return updateTs ? Long.valueOf(updateTs) : null
+  public void setId(String id) {
+    super.put("id", id);
   }
 
   @Override
-  void setLastModified(Long lastModified) {
-    super.put("updateTs", lastModified.toString())
+  @JsonIgnore
+  public Long getLastModified() {
+    String updateTs = DefaultGroovyMethods.asType(super.get("updateTs"), String.class);
+    return Strings.isNullOrEmpty(updateTs) ? null : Long.valueOf(updateTs);
   }
 
   @Override
-  String getLastModifiedBy() {
-    return super.get("lastModifiedBy")
+  public void setLastModified(Long lastModified) {
+    super.put("updateTs", lastModified.toString());
   }
 
   @Override
-  void setLastModifiedBy(String lastModifiedBy) {
-    super.put("lastModifiedBy", lastModifiedBy)
+  public String getLastModifiedBy() {
+    return (String) super.get("lastModifiedBy");
+  }
+
+  @Override
+  public void setLastModifiedBy(String lastModifiedBy) {
+    super.put("lastModifiedBy", lastModifiedBy);
   }
 
   @JsonIgnore
-  Object getConfig() {
-    return super.get("config")
+  public Object getConfig() {
+    return super.get("config");
   }
 
   @JsonIgnore
-  void setConfig(Object config) {
-    super.put("config", config)
+  public void setConfig(Object config) {
+    super.put("config", config);
   }
 
   @JsonIgnore
-  String getType() {
-    return super.get("type")
+  public String getType() {
+    return (String) super.get("type");
   }
 
   @JsonIgnore
-  void setType(String type) {
-    super.put("type", type)
+  public void setType(String type) {
+    super.put("type", type);
   }
 
   @JsonIgnore
-  Collection<Trigger> getTriggers() {
-    return mapper.convertValue(super.getOrDefault("triggers", new ArrayList<>()), Trigger.COLLECTION_TYPE)
+  public Collection<Trigger> getTriggers() {
+    return MAPPER.convertValue(
+        super.getOrDefault("triggers", new ArrayList<>()), Trigger.COLLECTION_TYPE);
   }
 
-  void setTriggers(Collection<Trigger> triggers) {
+  public void setTriggers(Collection<Trigger> triggers) {
     this.put("triggers", triggers);
   }
 
+  /**
+   * Denotes templated pipeline config schema version.
+   *
+   * @return
+   */
   @JsonIgnore
-  String getSchema() {
-    return super.get("schema") ?: "1" // NOTE: Denotes templated pipeline config schema version.
+  public String getSchema() {
+    final String get = (String) super.get("schema");
+    return Strings.isNullOrEmpty(get) ? "1" : get;
   }
 
   @JsonIgnore
-  void setSchema(String schema) {
-    super.put("schema", schema)
+  public void setSchema(String schema) {
+    super.put("schema", schema);
   }
 
   @JsonIgnore
-  Integer getIndex() {
-    return (Integer) super.get("index")
+  public Integer getIndex() {
+    return (Integer) super.get("index");
   }
 
   @JsonIgnore
-  void setIndex(Integer index) {
-    super.put("index", index)
+  public void setIndex(Integer index) {
+    super.put("index", index);
   }
 }
