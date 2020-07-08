01d07b8d1308a6088e35dc6e661eab9dba3ae32b
CyrusKOCI <66336602+CyrusKOCI@users.noreply.github.com>
2020-07-08 12:49:27 -0700

    Removed comment requested by maintainer

--- front50-oracle/src/main/java/com/netflix/spinnaker/front50/model/OracleStorageService.java
+++ front50-oracle/src/main/java/com/netflix/spinnaker/front50/model/OracleStorageService.java
@@ -6,8 +6,6 @@
  * If a copy of the Apache License Version 2.0 was not distributed with this file,
  * You can obtain one at https://www.apache.org/licenses/LICENSE-2.0.html
  */
-// changed timeCreated() to timeModified() in code below to fix pipeline issue
-
 package com.netflix.spinnaker.front50.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
