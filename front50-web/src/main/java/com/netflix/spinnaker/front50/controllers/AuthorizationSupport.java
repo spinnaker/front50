150d7f9bb77c6b9b75a4c40755a8146375f6c196
KeisukeYamashita <19yamashita15@gmail.com>
2020-11-17 19:13:12 +0900

    fix(core): revert runAsuser check in preauth

--- front50-web/src/main/java/com/netflix/spinnaker/front50/controllers/AuthorizationSupport.java
+++ front50-web/src/main/java/com/netflix/spinnaker/front50/controllers/AuthorizationSupport.java
@@ -50,7 +50,7 @@ public class AuthorizationSupport {
             .orElse(Collections.emptyList());
 
     if (runAsUsers.isEmpty()) {
-      return false;
+      return true;
     }
 
     final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
