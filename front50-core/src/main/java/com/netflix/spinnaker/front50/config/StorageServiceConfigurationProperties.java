69c8af40b1f4bbd39342f9b7529de702b48da7c3
David Byron <dbyron@salesforce.com>
2023-04-14 07:50:41 -0700

    refactor(core): set defaults for StorageServiceConfigurationProperties via setters instead of a constructor
    to make it easier to add more properties

--- front50-core/src/main/java/com/netflix/spinnaker/front50/config/StorageServiceConfigurationProperties.java
+++ front50-core/src/main/java/com/netflix/spinnaker/front50/config/StorageServiceConfigurationProperties.java
@@ -1,44 +1,86 @@
 package com.netflix.spinnaker.front50.config;
 
 import java.util.concurrent.TimeUnit;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("storage-service")
 @Data
 public class StorageServiceConfigurationProperties {
 
-  private PerObjectType application = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType applicationPermission = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType serviceAccount = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType project = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType notification = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType pipelineStrategy = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType pipeline = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType pipelineTemplate = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType snapshot = new PerObjectType(2, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType deliveryConfig = new PerObjectType(20, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType pluginInfo = new PerObjectType(2, TimeUnit.MINUTES.toMillis(1));
-  private PerObjectType entityTags = new PerObjectType(2, TimeUnit.MINUTES.toMillis(5), false);
+  private PerObjectType application =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType applicationPermission =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType serviceAccount =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType project =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType notification =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType pipelineStrategy =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType pipeline =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType pipelineTemplate =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType snapshot =
+      new PerObjectType()
+          .setThreadPool(2)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType deliveryConfig =
+      new PerObjectType()
+          .setThreadPool(20)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType pluginInfo =
+      new PerObjectType()
+          .setThreadPool(2)
+          .setRefreshMs(TimeUnit.MINUTES.toMillis(1))
+          .setShouldWarmCache(true);
+  private PerObjectType entityTags =
+      new PerObjectType().setThreadPool(2).setRefreshMs(TimeUnit.MINUTES.toMillis(5));
 
   @Data
-  @AllArgsConstructor
+  @Accessors(chain = true)
   public static class PerObjectType {
     private int threadPool;
     private long refreshMs;
     private boolean shouldWarmCache;
 
-    public PerObjectType(int threadPool, long refreshMs) {
-      this(threadPool, refreshMs, true);
-    }
-
-    public void setThreadPool(int threadPool) {
+    public PerObjectType setThreadPool(int threadPool) {
       if (threadPool <= 1) {
         throw new IllegalArgumentException("threadPool must be >= 1");
       }
 
       this.threadPool = threadPool;
+      return this;
     }
   }
 }
